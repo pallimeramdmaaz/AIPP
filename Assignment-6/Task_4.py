dae6371fbf4e6a7c1d510adca4ab93e78133983e
pallimeramdmaaz <mohammedmaaz898@gmail.com>
2025-11-23 21:54:55 +0530

    Add age classification based on user input

---
+++ Assignment-6/Task_4.py
@@ -0,0 +1,12 @@
+age = int(input("Enter your age: "))
+
+if age < 0:
+    print("Invalid age")
+elif age <= 12:
+    print("You are a child.")
+elif age <= 19:
+    print("You are a teenager.")
+elif age <= 64:
+    print("You are an adult.")
+else:
+    print("You are a senior.")
